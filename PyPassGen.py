ff49c5d53ee58a81109edfd3065db546d7552f36
AntonisKoukou <47377727+AntonisKoukou@users.noreply.github.com>
2019-03-24 16:33:42 +0200

    Add files via upload

---
+++ PyPassGen.py
@@ -0,0 +1,25 @@
+print("Welcome to PyPassGen v1.0.1")
+print("\n") 
+name = input("Enter a random name or word: ") 
+num = input("Enter a number: ") 
+print("\n") 
+print("\n") 
+print("ant" + str(556)+ str(65)+ name + str(665) + str(num) + "nis") 
+print("\n") 
+print("by" + str(4558) + name + str(num) + "play")
+print("\n")
+print("dhfhfhf" + str(4558.558-56) + name + str(num) + "playHSPp")
+print("\n") 
+print("pers" + str(-+55.4558) + name + str(num) + "nalle")
+print("\n") 
+print("\n") 
+print("Chose one that is for you.") 
+
+
+
+
+
+
+
+
+
